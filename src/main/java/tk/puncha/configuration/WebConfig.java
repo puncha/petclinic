e996e3f3d80c0ce9a7fd533f53adf7b458c5512f
PunCha <punchapuncha@gmail.com>
2016-03-28 01:20:17 +0800

    Static Home page is added.

---
+++ src/main/java/tk/puncha/configuration/WebConfig.java
@@ -0,0 +1,8 @@
+package tk.puncha.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
