37f6ef4e70be961de177bdbb970b7a4876ae9444
puncha <puncha.info@gmail.com>
2016-08-16 23:22:41 +0800

    Add dummy test case to avoid jUnit reports errors for empty base test class

--- src/test/java/tk/puncha/integration/webapp/ControllerTests.java
+++ src/test/java/tk/puncha/integration/webapp/ControllerTests.java
@@ -2,7 +2,6 @@ package tk.puncha.integration.webapp;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
@@ -12,9 +11,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase
-@TestPropertySource(locations = {"/test.properties", "/integration-test.properties"})
+@TestPropertySource(locations = "classpath:test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ControllerTests {
+class ControllerTests {
 
   @Value("${local.server.port}")
   private String PORT;
@@ -47,10 +46,5 @@ public class ControllerTests {
 
   HtmlPage getPage(String path) throws java.io.IOException {
     return webClient.getPage(SERVER + ":" + PORT + path);
-  }
-
-  @Test
-  public void dummy() throws Exception {
-    // Make jUnit happy
   }
 }
