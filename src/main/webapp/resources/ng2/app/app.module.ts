551416c11c32c16d0c79f6b531e12d005699aa72
PunCha <punchapuncha@gmail.com>
2016-09-10 09:35:35 +0800

    Move pets related stuff to PetsModule.

--- src/main/webapp/resources/ng2/app/app.module.ts
+++ src/main/webapp/resources/ng2/app/app.module.ts
@@ -6,23 +6,22 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {AppComponent} from "./app.component";
 import {NavComponent} from "./nav.component";
 import {FooterComponent} from "./footer.component";
-import {PetListComponent} from "./pets/pet-list.component";
 import {routing, appRoutingProviders} from "./app.routing";
 import {OwnersModule} from "./owners/owners.module";
 import {WelcomeModule} from "./welcome/welcome.module";
+import {PetModule} from "./pets/pets.module";
 
 @NgModule({
   imports: [
     BrowserModule, FormsModule, HttpModule, NgbModule,
-    routing, OwnersModule, WelcomeModule
+    routing, WelcomeModule, OwnersModule, PetModule
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent],
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent,
-    PetListComponent
+    FooterComponent
   ],
 
 })
--- src/main/webapp/resources/ng2/app/app.routing.ts
+++ src/main/webapp/resources/ng2/app/app.routing.ts
@@ -1,10 +1,8 @@
 import {Routes, RouterModule} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
-import {PetListComponent} from "./pets/pet-list.component";
 
 const appRoutes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/welcome'},
-  {path: 'pets', component: PetListComponent},
 ];
 
 export const appRoutingProviders: any[] = [];
